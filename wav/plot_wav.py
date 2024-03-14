791f5b2bf5984deee008b3e96677a989f8847780
Yoan <yoanivanov9@gmail.com>
2024-03-14 16:11:41 +0200

    dumb ploting

--- wav/plot_wav.py
+++ wav/plot_wav.py
@@ -1,30 +1,38 @@
+# https://stackoverflow.com/questions/64410196/how-to-plot-a-waveform-from-wav-file-in-python
+
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
 import numpy as np
 
-# https://stackoverflow.com/questions/64410196/how-to-plot-a-waveform-from-wav-file-in-python
+def plot_file(audioFilename):
+    sampleRate, audioBuffer = scipy.io.wavfile.read(audioFilename)
 
-myAudioFilename = 'sample_audio/output.wav'
-sampleRate, audioBuffer = scipy.io.wavfile.read(myAudioFilename)
+    duration = len(audioBuffer) / sampleRate
+    time = np.arange(0, duration, 1/sampleRate)
 
-duration = len(audioBuffer)/sampleRate
-time = np.arange(0,duration,1/sampleRate)
+    plt.figure()
+    plt.plot(time, audioBuffer)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.title(audioFilename)
 
-plt.plot(time,audioBuffer)
-plt.xlabel('Time [s]')
-plt.ylabel('Amplitude')
-plt.title(myAudioFilename)
+def plot_files(audioFilenames):
+    for audioFilename in audioFilenames:
+        sampleRate, audioBuffer = scipy.io.wavfile.read(audioFilename)
 
-# myAudioFilename = 'sample_audio/output_inverted.wav'
-# sampleRate, audioBuffer = scipy.io.wavfile.read(myAudioFilename)
+        duration = len(audioBuffer) / sampleRate
+        time = np.arange(0, duration, 1/sampleRate)
 
-# duration = len(audioBuffer)/sampleRate
-# time = np.arange(0,duration,1/sampleRate)
+        plt.plot(time, audioBuffer)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Amplitude')
+        plt.title(audioFilename)
 
-# plt.plot(time,audioBuffer)
-# plt.xlabel('Time [s]')
-# plt.ylabel('Amplitude')
-# plt.title(myAudioFilename)
+# Plot each file in a separate window
+plot_file('sample_audio/output.wav')
+plot_file('sample_audio/output_inverted.wav')
+plot_file('sample_audio/output_inverted.wav')
 
+plot_files(['sample_audio/output.wav', 'sample_audio/output_inverted.wav'])
 
 plt.show()
