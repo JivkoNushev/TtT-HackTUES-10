1416ac81e12a558c71e7a25baca993cb85aa0618
Yoan <yoanivanov9@gmail.com>
2024-03-14 16:20:44 +0200

    fixed ploting

--- wav/plot_wav.py
+++ wav/plot_wav.py
@@ -1,38 +1,40 @@
-# https://stackoverflow.com/questions/64410196/how-to-plot-a-waveform-from-wav-file-in-python
-
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
 import numpy as np
 
-def plot_file(audioFilename):
+
+def plot_single_file(audioFilename):
     sampleRate, audioBuffer = scipy.io.wavfile.read(audioFilename)
 
     duration = len(audioBuffer) / sampleRate
     time = np.arange(0, duration, 1/sampleRate)
 
-    plt.figure()
+    plt.figure()  # Create a new figure for each plot
     plt.plot(time, audioBuffer)
     plt.xlabel('Time [s]')
     plt.ylabel('Amplitude')
     plt.title(audioFilename)
 
-def plot_files(audioFilenames):
+
+def plot_multiple_files(audioFilenames):
+    plt.figure()  # Create a new figure for all plots
     for audioFilename in audioFilenames:
         sampleRate, audioBuffer = scipy.io.wavfile.read(audioFilename)
 
         duration = len(audioBuffer) / sampleRate
         time = np.arange(0, duration, 1/sampleRate)
 
-        plt.plot(time, audioBuffer)
-        plt.xlabel('Time [s]')
-        plt.ylabel('Amplitude')
-        plt.title(audioFilename)
+        plt.plot(time, audioBuffer, label=audioFilename)
 
-# Plot each file in a separate window
-plot_file('sample_audio/output.wav')
-plot_file('sample_audio/output_inverted.wav')
-plot_file('sample_audio/output_inverted.wav')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Amplitude')
+    plt.title('Waveform Comparison')
+    plt.legend()
 
-plot_files(['sample_audio/output.wav', 'sample_audio/output_inverted.wav'])
+
+plot_single_file('sample_audio/output.wav')
+plot_single_file('sample_audio/output_inverted.wav')
+
+plot_multiple_files(['sample_audio/output.wav', 'sample_audio/output_inverted.wav'])
 
 plt.show()
