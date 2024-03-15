738b41a6ad7c23ec1823722b29a8dd9d2974cd13
Yoan <yoanivanov9@gmail.com>
2024-03-15 15:21:03 +0200

    displaying waves

--- web_interface/app/controllers/audios_controller.rb
+++ web_interface/app/controllers/audios_controller.rb
@@ -3,15 +3,7 @@ class AudiosController < ApplicationController
     @audios = Audio.all
   end
 
-  def create
-    audio_files = Dir.glob("#{Audio.audio_folder_path}/*.wav")
-
-    audio_files.each do |file|
-      audio = Audio.new
-      audio.audio_file.attach(io: File.open(file), filename: File.basename(file))
-      audio.save
-    end
-
-    redirect_to root_path, notice: 'Audio files uploaded successfully.'
+  def show
+    @audio = Audio.find(params[:id])
   end
 end
